4798a9d96c6e13004458ba79e1c4d7db7e93f477
nathan denholm <nathandenholm@live.co.uk>
2020-10-08 14:04:08 +0100

    Add initial project boilerplate

---
+++ src/app/components/ticker-list/ticker-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticker-list',
+  templateUrl: './ticker-list.component.html',
+  styleUrls: ['./ticker-list.component.scss']
+})
+export class TickerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
