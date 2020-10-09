d73d33563f9882cc24f7f22564eac4c34b7d5e29
nathan denholm <nathandenholm@live.co.uk>
2020-10-10 00:33:03 +0100

    add case where a single date can be passed

--- src/app/services/data-fetch/data-fetching.service.ts
+++ src/app/services/data-fetch/data-fetching.service.ts
@@ -8,24 +8,33 @@ interface response {
 	date:string
 }
 
+
+
 @Injectable({
 	providedIn: 'root'
 })
 export class DataFetchingService {
-
+	
 	baseUrlHistory:string = "https://api.exchangeratesapi.io/history";
 	baseUrlLatest:string = "https://api.exchangeratesapi.io/latest";
 	
 	constructor(private request: HttpClient) { }
 
-	fetchCurrencyData(history:boolean, params?:any):Observable<response>{
+	fetchCurrencyData(history:boolean, params?:any, date?:string,):Observable<response>{
 		if(history){
-			if(!params.startDate){
+			// fetch data based on date range
+			if(!params.start_at){
 				throw new Error("Start Date Missing");
 			}
 			return this.request.get<response>(this.baseUrlHistory, {params:params});
+		}
+		else if(date){
+			// fetch data based on one day
+			return this.request.get<response>(`https://api.exchangeratesapi.io/${date}`, {params:params})
+			
 		}	
 		else{
+			// fetch latest data
 			return this.request.get<response>(this.baseUrlLatest, {params:params});
 		}
 	}
