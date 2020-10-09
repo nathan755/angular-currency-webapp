e99694b42c5e929343dd17e7a46fddc84c71c8b9
nathan denholm <nathandenholm@live.co.uk>
2020-10-09 16:48:50 +0100

    add function to calculate currency conversion

--- src/app/components/currency-converter/currency-converter.component.ts
+++ src/app/components/currency-converter/currency-converter.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { DataFetchingService } from "../../services/data-fetch/data-fetching.service";
-
+//!! unsubsscibe ????
 @Component({
 	selector: 'app-currency-converter',
 	templateUrl: './currency-converter.component.html',
@@ -12,6 +12,7 @@ export class CurrencyConverterComponent implements OnInit {
 	tickers:string[] = [];
 	rates:object;
 	error:boolean = false;
+	result:number;
 
 	amount = new FormControl();
 	currencyOne = new FormControl();
@@ -19,16 +20,32 @@ export class CurrencyConverterComponent implements OnInit {
 	
 	constructor(private dataFetchingService:DataFetchingService ) { }
 
-	onConvertClick(){}
-	
 	ngOnInit(): void {
+		// fetch tickers for selects.
 		this.dataFetchingService.fetchCurrencyData(false).subscribe((res) => {
-			this.rates = res.rates;
 			this.tickers = Object.keys(res.rates)
 		},
 			(err) => {
 				console.log("err", err)
 				this.error = true;
 			});
-		}
+	}
+
+	onConvertClick():void{
+		this.convertCurrency(this.currencyOne.value, this.currencyTwo.value);
+	}
+
+	convertCurrency(currencyOne: string, currencyTwo: string):void {
+		// fetch currency rates for currencyOne.
+		this.dataFetchingService.fetchCurrencyData(false, { base: currencyOne }).subscribe(
+			(res) => {
+				this.result = this.amount.value * res.rates[currencyTwo];
+			},
+			(err) => {
+				console.log("err", err);
+				this.error = true;
+			});
+	}
+	
+
 }
