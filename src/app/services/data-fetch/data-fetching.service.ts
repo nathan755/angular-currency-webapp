911f9ce8fe543e4610f35a14ccfdce9e857ab747
nathan denholm <nathandenholm@live.co.uk>
2020-10-09 12:40:23 +0100

    add a response type interface

--- src/app/services/data-fetch/data-fetching.service.ts
+++ src/app/services/data-fetch/data-fetching.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+interface response {
+	rates:object
+	base:string,
+	date:string
+}
+
 @Injectable({
 	providedIn: 'root'
 })
@@ -12,15 +18,15 @@ export class DataFetchingService {
 	
 	constructor(private request: HttpClient) { }
 
-	fetchCurrencyData(history:boolean, params?:any):Observable<object>{
+	fetchCurrencyData(history:boolean, params?:any):Observable<response>{
 		if(history){
 			if(!params.startDate){
 				throw new Error("Start Date Missing");
 			}
-			return this.request.get(this.baseUrlHistory, {params:params});
+			return this.request.get<response>(this.baseUrlHistory, {params:params});
 		}	
 		else{
-			return this.request.get(this.baseUrlLatest, {params:params});
+			return this.request.get<response>(this.baseUrlLatest, {params:params});
 		}
 	}
 }
