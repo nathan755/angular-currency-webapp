ecbe483c44bfe67b2a2691f1457864db202e4b7e
nathan denholm <nathandenholm@live.co.uk>
2020-10-09 12:41:12 +0100

    add currency converter component

--- src/app/components/currency-converter/currency-converter.component.ts
+++ src/app/components/currency-converter/currency-converter.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DataFetchingService } from "../../services/data-fetch/data-fetching.service";
 
 @Component({
-  selector: 'app-currency-converter',
-  templateUrl: './currency-converter.component.html',
-  styleUrls: ['./currency-converter.component.scss']
+	selector: 'app-currency-converter',
+	templateUrl: './currency-converter.component.html',
+	styleUrls: ['./currency-converter.component.scss']
 })
 export class CurrencyConverterComponent implements OnInit {
+	
+	tickers:string[] = [];
+	rates:object;
+	error:boolean = false;
 
-  constructor() { }
+	amount = new FormControl();
+	currencyOne = new FormControl();
+	currencyTwo = new FormControl();
+	
+	constructor(private dataFetchingService:DataFetchingService ) { }
 
-  ngOnInit(): void {
-  }
-
+	onConvertClick(){}
+	
+	ngOnInit(): void {
+		this.dataFetchingService.fetchCurrencyData(false).subscribe((res) => {
+			this.rates = res.rates;
+			this.tickers = Object.keys(res.rates)
+		},
+			(err) => {
+				console.log("err", err)
+				this.error = true;
+			});
+		}
 }
